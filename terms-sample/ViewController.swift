e0f4e7933f009db26bd199fb6f30ecb0847ac716
karamawariken <24karamawariken@gmail.com>
2019-08-02 08:37:33 +0900

    Initial Commit

---
+++ terms-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  terms-sample
+//
+//  Created by nishi kosei on 2019/08/02.
+//  Copyright © 2019 nishi kosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ terms-sampleTests/terms_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  terms_sampleTests.swift
+//  terms-sampleTests
+//
+//  Created by nishi kosei on 2019/08/02.
+//  Copyright © 2019 nishi kosei. All rights reserved.
+//
+
+import XCTest
+@testable import terms_sample
+
+class terms_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
