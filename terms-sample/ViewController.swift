1a1df3649f2ca4b44d3b6e58e6d85e3405130d99
karamawariken <24karamawariken@gmail.com>
2019-08-03 22:52:29 +0900

    チェックボタンのロジック作成 次に進むボタンのデザイン変更ロジック作成

--- terms-sample/ViewController.swift
+++ terms-sample/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// 利用規約ボタンUI
+    @IBOutlet weak var consentButton1: UIButton!
+    @IBOutlet weak var consentButton2: UIButton!
+    
+    //次に進むボタンUI
+    @IBOutlet weak var nextPageButton: UIButton!
+    
+    private let checkedImage = UIImage(named: "check_on")
+    private let uncheckedImage = UIImage(named: "check_off")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        /// Button1の設定
+        self.consentButton1.setImage(uncheckedImage, for: .normal)
+        self.consentButton1.setImage(checkedImage, for: .selected)
+        
+        /// Button2の設定
+        self.consentButton2.setImage(uncheckedImage, for: .normal)
+        self.consentButton2.setImage(checkedImage, for: .selected)
+        
+        checkNextButtonenable()
     }
 
+    @IBAction func consentButton1DidTap(_ sender: Any) {
+        /// 選択状態を反転させる
+        self.consentButton1.isSelected = !self.consentButton1.isSelected
+        checkNextButtonenable()
+    }
+    
+    @IBAction func consentButton2DidTap(_ sender: Any) {
+        /// 選択状態を反転させる
+        self.consentButton2.isSelected = !self.consentButton2.isSelected
+        checkNextButtonenable()
+    }
+    
+    func checkNextButtonenable(){
+        /// どちらも同意チェック済みか判断する
+        if (self.consentButton1.isSelected && self.consentButton2.isSelected){
+            self.nextPageButton.tintColor = UIColor.white
+            self.nextPageButton.layer.cornerRadius = 5
+            self.nextPageButton.layer.borderWidth = 1
+            self.nextPageButton.layer.backgroundColor = UIColor.blue.cgColor
+        } else {
+            self.nextPageButton.tintColor = UIColor.white
+            self.nextPageButton.layer.cornerRadius = 5
+            self.nextPageButton.layer.borderWidth = 1
+            self.nextPageButton.layer.backgroundColor = UIColor.gray.cgColor
 
+        }
+    }
+    
+    @IBAction func nextPageButtonDidTap(_ sender: Any) {
+        /// どちらも同意チェック済みの場合のみアクションさせる
+        if(self.consentButton1.isSelected && self.consentButton2.isSelected){
+            print("次に進めます")
+        }
+    }
 }
 
